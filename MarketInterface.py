09c860ce3cab6976754b95f448672400d74ac16f
Justine Craig <justincrg@gmail.com>
2021-07-04 14:58:54 -0700

    Added importing inventories to market interface

--- MarketInterface.py
+++ MarketInterface.py
@@ -1,10 +1,12 @@
+from Inventory import Inventory
+
 class MarketInterface:
-	buyInventory
-	sellInventory
-	moneyInventory
-	requestedLevel
-	yesterdaysPrice
-	wasOrderCompleted
+	buyInventory = Inventory()
+	sellInventory = Inventory()
+	moneyInventory = Inventory()
+	requestedLevel = 5
+	yesterdaysPrice = 10 #I don't really want to set this value to zero
+	wasOrderCompleted = False
 	def PlaceBuyOrder(self):
 		pass
 	def PlaceSellOrder(self):
