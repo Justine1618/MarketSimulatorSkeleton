e2710afa181fc8529a62b1dc0329c81b9334743d
Justine Craig <justincrg@gmail.com>
2021-07-04 14:53:01 -0700

    Added MarketInterface Python File

---
+++ MarketInterface.py
@@ -0,0 +1,13 @@
+class MarketInterface:
+	buyInventory
+	sellInventory
+	moneyInventory
+	requestedLevel
+	yesterdaysPrice
+	wasOrderCompleted
+	def PlaceBuyOrder(self):
+		pass
+	def PlaceSellOrder(self):
+		pass
+	def AdjustPrice(self):
+		pass
