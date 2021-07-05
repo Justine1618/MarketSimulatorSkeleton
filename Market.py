3928c5e95eabf2ec911a7f860cb6f074f7786ef8
Justine Craig <justincrg@gmail.com>
2021-07-05 14:18:44 -0700

    Added Market class file

---
+++ Market.py
@@ -0,0 +1,10 @@
+from Inventory import Inventory
+class Market:
+	sellOrders = Inventory()
+	buyOrders = Inventory()
+	def PlaceBuyOrder(self):
+		pass
+	def PlaceSellOrder(self):
+		pass
+	def Resolvetransaction(self):
+		pass
